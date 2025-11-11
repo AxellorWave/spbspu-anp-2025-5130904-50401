3ce78b8dda235903a9d91e3d53b0fafc59d093b9
Yugo <95368001+Degr3e@users.noreply.github.com>
2025-11-12 00:52:04 +0300

    add write and read matrix functions

--- yalovsky.gleb/P3/main.cpp
+++ yalovsky.gleb/P3/main.cpp
@@ -16,6 +16,26 @@ namespace yalovsky{
     }
     return true;
   }
+
+  void readMatrix(std::istream & in, int * matrix, size_t rows, size_t cols)
+  {
+    for (size_t i = 0; i < rows * cols; i++) {
+      in >> matrix[i];
+      if(!in) {
+       throw std::runtime_error("Invalid matrix data");
+      }
+    }
+  }
+
+  void writeMatrix(std::ostream & out, const int * matrix, size_t rows, size_t cols)
+  {
+    out << rows << " " << cols;
+    for (size_t i = 0; i < rows * cols; i++) {
+      out << " " << matrix[i];
+    }
+    out << "\n";
+  }
+
 }
 
 int main()
