5683da79fd7d9ea8d3b367bcf4f585a2ce33b94d
Yugo <95368001+Degr3e@users.noreply.github.com>
2025-11-11 22:42:17 +0300

    add isNumber function

--- yalovsky.gleb/P3/main.cpp
+++ yalovsky.gleb/P3/main.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+namespace yalovsky{
+
+  bool isNumber(const char * str)
+  {
+    if (!str || *str == '\0') {
+      return false;
+    }
+    for (size_t i = 0; str[i] != '\0'; i++) {
+      if (str[i] < '0' || str[i] > '9') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+} 
+
 int main()
 {
   return 0;
