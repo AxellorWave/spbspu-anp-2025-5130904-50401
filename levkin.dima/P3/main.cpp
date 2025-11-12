ffc7397eeb771bb883a95315af782c6f324dd3b6
qiwi <qiwi@qiwi.qiwi>
2025-11-12 13:10:42 +0300

    created a struct framework

--- levkin.dima/P3/main.cpp
+++ levkin.dima/P3/main.cpp
@@ -1,3 +1,31 @@
-int main () {
+#include <iostream>
+#include <fstream>
+
+
+
+
+
+namespace levin {
+    enum class MemoryType : int {
+        STATIC_MEMORY = 1,
+        DYNAMIC_MEMORY
+    };
+
+    struct Memory {
+        int* buffer;  
+
+        void createBuffer() {}
+        void clearBuffer() {}
+        int* LFT_BOT_CNT(); 
+    }; 
+} 
+
+int main (int argc, char ** argv) {
+    if (argc != 4 || !(argv[1][0] == '1' || argv[1][0] == '2')) {
+        std::cout << "wrong arg input\n";
+        return 1;
+    }
     
+    std::ifstream input(argv[2]);
+    std::ofstream ouput(argv[3]);
 }
