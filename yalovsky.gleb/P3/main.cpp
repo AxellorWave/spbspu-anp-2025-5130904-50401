b2020c0988409e007cddfe346b44973bb5ccd719
Yugo <95368001+Degr3e@users.noreply.github.com>
2025-11-14 01:00:03 +0300

    added function LFT-BOT-CNT

--- yalovsky.gleb/P3/main.cpp
+++ yalovsky.gleb/P3/main.cpp
@@ -81,6 +81,44 @@ namespace yalovsky{
       }
     }
   }
+
+  void spiralTransformLFTBOTCNT(int * matrix, size_t rows, size_t cols)
+  {
+    if (rows == 0 || cols == 0) {
+      return;
+    }
+
+    int count = 1;
+    size_t top = 0;
+    size_t bottom = rows - 1;
+    size_t left = 0;
+    size_t right = cols - 1;
+
+    while (top <= bottom && left <= right) {
+      for (size_t i = left; i <= right; i++) {
+        matrix[bottom * cols + i] += count++;
+      }
+      bottom--;
+
+      for (size_t j = bottom; j >= top && j <= rows - 1; j--) {
+        matrix[j * cols + right] += count++;
+      }
+      right--;
+
+      if (top <= bottom) {
+        for (size_t i = right; i >= left && i <= cols - 1; i--) {
+          matrix[top * cols + i] += count++;
+        }
+        top++;
+      }
+      if (left <= right) {
+        for (size_t i = top; i <= bottom; i++){
+          matrix[i * cols + left] += count++;
+        }
+        left++;
+      }
+    }
+  }
 }
 
 int main()
