42e5f577d6ffd7c0273d05484a517e47e4ad375e
elizazha <lizokbella@mail.ru>
2025-11-15 16:46:47 +0300

    +namespace

--- zhuravleva.elizaveta/P3/main.cpp
+++ zhuravleva.elizaveta/P3/main.cpp
@@ -2,79 +2,82 @@
 #include <fstream>
 #include <string>
 
-bool isNumber(const std::string& str)
-{
-	if (str.empty())
+namespace zhuravleva {
+	const int Max_size = 10000;
+  bool isNumber(const std::string& str)
   {
-    return false;
-  }
-	for (char c : str)
-  {
-		if (!isdigit(c))
+    if (str.empty())
     {
       return false;
     }
-	}
-	return true;
-}
-
-int cols_no_dublicats(int ** matrix, size_t rows, size_t cols)
-{
-	int count = 0;
-	for (size_t j = 0; j < cols; j++)
-  {
-		bool is_dublics = false;
-		for (size_t i = 0; i < rows - 1; i++)
+    for (char c : str)
     {
-			if (matrix[i][j] == matrix[i + 1][j])
+      if (!isdigit(c))
       {
-        is_dublics = true;
+        return false;
       }
-		}
-		if (!is_dublics)
-    {
-      count++;
     }
-	}
-	return count;
-}
-int diagonals_no_zero(int** matrix, size_t rows, size_t cols)
-{
-	int count = 0;
-	if (rows == 0 && cols == 0)
-  {
-    return 0;
+    return true;
   }
-	for (size_t shift = 1; shift < cols; shift++)
-  {
-		bool has_zero = false;
-		for (size_t i = 0; i < rows && i + shift < cols; i++)
-    {
-			if (matrix[i][i + shift] == 0)
-      {
-        has_zero = true;
-      }
-		}
-		if (!has_zero)
-    {
-      count++;
-    }
-	}
 
-	for (size_t shift = 1; shift < rows; shift++)
+  int cols_no_dublicats(int ** matrix, size_t rows, size_t cols)
   {
-		bool has_zero = false;
-		for (size_t i = 0; i < cols && i + shift < rows; i++)
+    int count = 0;
+    for (size_t j = 0; j < cols; j++)
     {
-			if (matrix[i+shift][i] == 0)
+      bool is_dublics = false;
+      for (size_t i = 0; i < rows - 1; i++)
       {
-        has_zero = true;
+        if (matrix[i][j] == matrix[i + 1][j])
+        {
+          is_dublics = true;
+        }
       }
-		}
-		if (!has_zero)
-    {
-      count++;
+      if (!is_dublics)
+      {
+        count++;
+      }
     }
-	}
-	return count;
+    return count;
+  }
+  int diagonals_no_zero(int** matrix, size_t rows, size_t cols)
+  {
+    int count = 0;
+    if (rows == 0 && cols == 0)
+    {
+      return 0;
+    }
+    for (size_t shift = 1; shift < cols; shift++)
+    {
+      bool has_zero = false;
+      for (size_t i = 0; i < rows && i + shift < cols; i++)
+      {
+        if (matrix[i][i + shift] == 0)
+        {
+          has_zero = true;
+        }
+      }
+      if (!has_zero)
+      {
+        count++;
+      }
+    }
+
+    for (size_t shift = 1; shift < rows; shift++)
+    {
+      bool has_zero = false;
+      for (size_t i = 0; i < cols && i + shift < rows; i++)
+      {
+        if (matrix[i+shift][i] == 0)
+        {
+          has_zero = true;
+        }
+      }
+      if (!has_zero)
+      {
+        count++;
+      }
+    }
+    return count;
+  }
 }
