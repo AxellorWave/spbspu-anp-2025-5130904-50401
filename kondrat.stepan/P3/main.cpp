c4d9081e880bd79b631e7d31adf4e915c84effab
StKondrat <kndrt13@icloud.com>
2025-11-15 21:32:49 +0300

    rework old checks, start using namespace, add new checks, start create array

--- kondrat.stepan/P3/main.cpp
+++ kondrat.stepan/P3/main.cpp
@@ -5,37 +5,141 @@
 
 namespace kondrat
 {
+  bool check_args(int argc, char ** argv, int & mode)
+  {
+    if (argc < 4)
+    {
+      std::cerr << "Not enough arguments\n";
+      return false;
+    }
+    else if (argc > 4)
+    {
+      std::cerr << "Too many arguments\n";
+      return false;
+    }
 
+    char * endptr = nullptr;
+    long val = std::strtol(argv[1], &endptr, 10);
+
+    if (*endptr != '\0')
+    {
+      std::cerr << "First parameter is not a number\n";
+      return false;
+    }
+
+    if (val != 1 && val != 2)
+    {
+      std::cerr << "First parameter is out of range\n";
+      return false;
+    }
+
+    mode = static_cast<int>(val);
+    return true;
+  }
+
+  bool read_matrix_dimensions(std::ifstream & fin, size_t & rows, size_t & cols)
+  {
+    long long tmp_r = 0, tmp_c = 0;
+
+    if (!(fin >> tmp_r >> tmp_c))
+    {
+      std::cerr << "Invalid matrix\n";
+      return false;
+    }
+
+    if (tmp_r < 0 || tmp_c < 0)
+    {
+      std::cerr << "Invalid matrix\n";
+      return false;
+    }
+
+    rows = static_cast<size_t>(tmp_r);
+    cols = static_cast<size_t>(tmp_c);
+    return true;
+  }
+
+  bool validate_matrix_elements(std::ifstream & fin, size_t rows, size_t cols)
+  {
+    size_t total = rows * cols;
+
+    for (size_t i = 0; i < total; i++)
+    {
+      int x;
+      if (!(fin >> x))
+      {
+        std::cerr << "Invalid matrix\n";
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool fill_matrix(std::ifstream & fin, int * m, size_t rows, size_t cols)
+  {
+    size_t total = rows * cols;
+    for (size_t i = 0; i < total; i++)
+    {
+      if (!(fin >> m[i]))
+      {
+        std::cerr << "Invalid matrix\n";
+        return false;
+      }
+    }
+    return true;
+  }
 }
-
-
 
 int main(int argc, char ** argv)
 {
-  if (argc < 4)
+  int mode = 0;
+
+  if (!kondrat::check_args(argc, argv, mode))
   {
-    std::cerr << "Not enough arguments\n";
-    return 1;
-  }
-  else if (argc > 4)
-  {
-    std::cerr << "Too many arguments";
     return 1;
   }
 
-  int num = 0;
-  try
+  std::ifstream fin(argv[2]);
+  if (!fin)
   {
-    num = std::stoi(argv[1]);
+    std::cerr << "Cannot open input file\n";
+    return 2;
   }
-  catch(const std::invalid_argument&)
+
+  size_t rows = 0, cols = 0;
+  if (!kondrat::read_matrix_dimensions(fin, rows, cols))
   {
-    std::cerr << "First parameter is not a number\n";
-    return 1;
+    return 2;
   }
-  if (num != 1 && num != 2)
+
+  if (!kondrat::validate_matrix_elements(fin, rows, cols))
   {
-    std::cerr << "First parameter is out of range\n";
-    return 1;
+    return 2;
+  }
+
+  int * m = nullptr;
+  int m_stack[10000];
+
+  if (mode == 1)
+  {
+    m = m_stack;
+  }
+  else
+  {
+    m = reinterpret_cast<int *>(malloc(rows * cols * sizeof(int)));
+    if (!m)
+    {
+      std::cerr << "Bad allocation memory\n";
+      return 3;
+    }
+  }
+
+  fin.seekg(0);
+  long long tmp_r, tmp_c;
+  fin >> tmp_r >> tmp_c;
+
+  if (!kondrat::fill_matrix(fin, m, rows, cols))
+  {
+    if (mode == 2) free(m);
+    return 2;
   }
 }
