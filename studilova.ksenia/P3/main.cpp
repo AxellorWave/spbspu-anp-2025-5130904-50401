e21f0e080d60ab49ec923ba6271a4537319be477
kstudilova <kstudilova@gmail.com>
2025-11-16 01:30:12 +0300

    freeing memory for dynamic_matrix_t

--- studilova.ksenia/P3/main.cpp
+++ studilova.ksenia/P3/main.cpp
@@ -14,6 +14,17 @@ namespace studilova {
         int cols;
     };
 
+     void free_dynamic_matrix(dynamic_matrix_t* dm) {
+        if (!dm) {
+            return;
+        }
+        for (int i = 0; i < dm->rows; ++i) {
+            std::free(dm->data[i]);
+        }
+        std::free(dm->data);
+        std::free(dm);
+    }
+
     bool read_matrix(const char* filename, fixed_matrix_t* fm, dynamic_matrix_t** dm_ptr, int mode) {
         FILE* f = std::fopen(filename, "r");
         if (!f) {
