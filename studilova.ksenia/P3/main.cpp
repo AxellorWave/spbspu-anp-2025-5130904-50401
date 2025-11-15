ad16f676dd8feb74c3cfd1554baaedb99c151aa0
kstudilova <kstudilova@gmail.com>
2025-11-15 22:18:28 +0300

    add namespace

--- studilova.ksenia/P3/main.cpp
+++ studilova.ksenia/P3/main.cpp
@@ -1,4 +1,11 @@
-#include <iostream> 
-int main(){
-    
+#include <iostream>
+
+namespace studilova {
+    int main_impl(int argc, char* argv[]) {
+        return 0;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    return studilova::main_impl(argc, argv);
 }
