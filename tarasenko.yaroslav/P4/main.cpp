9a1ffede9a7aa9dde4396b346b6f164c3900320f
RapidMindd <YaroslavT007@yandex.ru>
2025-11-21 18:53:45 +0300

    namespace added

--- tarasenko.yaroslav/P4/main.cpp
+++ tarasenko.yaroslav/P4/main.cpp
@@ -2,97 +2,100 @@
 #include <iomanip>
 #include <cctype>
 
-bool is_vowel(char c)
+namespace tarasenko
 {
-  return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
-}
-
-void remove_vowels(const char * string, char * output, size_t len)
-{
-  size_t k = 0;
-  for (size_t i = 0; i < len; ++i)
+  bool is_vowel(char c)
   {
-    char current = string[i];
-    char lower_c = tolower(static_cast< unsigned char >(current));
-    if (is_vowel(lower_c))
-    {
-      continue;
-    }
-    output[k] = string[i];
-    k++;
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
   }
-}
 
-void upp_to_low(const char * string, char * output, size_t len)
-{
-  for (size_t i = 0; i < len; ++i)
+  void remove_vowels(const char * string, char * output, size_t len)
   {
-    char current = string[i];
-    if (std::isupper(static_cast< unsigned char >(current)))
+    size_t k = 0;
+    for (size_t i = 0; i < len; ++i)
     {
-      output[i] = std::tolower(static_cast< unsigned char >(current));
-    }
-    else
-    {
-      output[i] = string[i];
+      char current = string[i];
+      char lower_c = tolower(static_cast< unsigned char >(current));
+      if (is_vowel(lower_c))
+      {
+        continue;
+      }
+      output[k] = string[i];
+      k++;
     }
   }
-}
 
-char * getline(std::istream & in, size_t & len)
-{
-  bool is_skipws = in.flags() & std::ios_base::skipws;
-  if (is_skipws)
+  void upp_to_low(const char * string, char * output, size_t len)
   {
-    in >> std::noskipws;
-  }
-  char * string = nullptr;
-  size_t k = 0;
-  char last_symbol = '0';
-  while (in)
-  {
-    in >> last_symbol;
-    if (last_symbol == '\n')
+    for (size_t i = 0; i < len; ++i)
     {
-      break;
+      char current = string[i];
+      if (std::isupper(static_cast< unsigned char >(current)))
+      {
+        output[i] = std::tolower(static_cast< unsigned char >(current));
+      }
+      else
+      {
+        output[i] = string[i];
+      }
+    }
+  }
+
+  char * getline(std::istream & in, size_t & len)
+  {
+    bool is_skipws = in.flags() & std::ios_base::skipws;
+    if (is_skipws)
+    {
+      in >> std::noskipws;
+    }
+    char * string = nullptr;
+    size_t k = 0;
+    char last_symbol = '0';
+    while (in)
+    {
+      in >> last_symbol;
+      if (last_symbol == '\n')
+      {
+        break;
+      }
+      char * temp_mem = new char[k + 1];
+      for (size_t i = 0; i < k; ++i)
+      {
+        temp_mem[i] = string[i];
+      }
+      temp_mem[k] = last_symbol;
+      delete[] string;
+      string = temp_mem;
+      k++;
     }
     char * temp_mem = new char[k + 1];
     for (size_t i = 0; i < k; ++i)
     {
       temp_mem[i] = string[i];
     }
-    temp_mem[k] = last_symbol;
+    temp_mem[k] = '\0';
     delete[] string;
     string = temp_mem;
-    k++;
+    len = k;
+    if (is_skipws)
+    {
+      in >> std::skipws;
+    }
+    return string;
   }
-  char * temp_mem = new char[k + 1];
-  for (size_t i = 0; i < k; ++i)
-  {
-    temp_mem[i] = string[i];
-  }
-  temp_mem[k] = '\0';
-  delete[] string;
-  string = temp_mem;
-  len = k;
-  if (is_skipws)
-  {
-    in >> std::skipws;
-  }
-  return string;
 }
 
 int main()
 {
   size_t len = 0;
-  char * string = getline(std::cin, len);
+  char * string = tarasenko::getline(std::cin, len);
   char * lower_string = new char[len + 1]{};
-  upp_to_low(string, lower_string, len);
+  tarasenko::upp_to_low(string, lower_string, len);
   std::cout << string << '\n';
   std::cout << lower_string << '\n';
   delete[] lower_string;
   char * without_vowels = new char[len + 1]{};
-  remove_vowels(string, without_vowels, len);
+  tarasenko::remove_vowels(string, without_vowels, len);
   std::cout << without_vowels << '\n';
   delete[] without_vowels;
   delete[] string;
