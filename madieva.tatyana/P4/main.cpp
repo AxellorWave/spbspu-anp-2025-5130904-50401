978ac18215e977f4d981f5187585470d20fcebcb
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-11-22 22:39:03 +0300

    bug fixes

--- madieva.tatyana/P4/main.cpp
+++ madieva.tatyana/P4/main.cpp
@@ -20,20 +20,27 @@ int main() {
   char str1[max_size] = {};
   char a = 'a';
   size_t i = 0;
-  while(std::cin >> a) {
+  while(std::cin.get(a) && a != '\n') {
+    if (i = max_size - 1) {
+        return 2;
+    }
     str1[i] = a;
     i++;
   }
   if (std::cin.fail()) {
     return 1;
   }
-  char * str2 = {"def "};
-  while(str2 != '\0') {
-    i++;
+  const char * str2 = {"def "};
+  size_t j = 0;
+  while(str2[j] != '\0') {
+    j++;
   }
-  char * res = reinterpret_cast<char*>(malloc(sizeof(char) * i));
+  size_t size = i + j;
+  char * res = reinterpret_cast<char*>(malloc(sizeof(char) * size + 1));
   if (res == nullptr) {
     return 2;
   }
-  uni_two_merge(str1, str2, i, res);
+  uni_two_merge(str1, str2, size, res);
+  free(res);
+  return 0;
 }
