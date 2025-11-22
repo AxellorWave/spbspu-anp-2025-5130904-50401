a390010a57612d0c3187d5667cfc78ffb53e077a
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-11-22 21:58:23 +0300

    string filling and the main function

--- madieva.tatyana/P4/main.cpp
+++ madieva.tatyana/P4/main.cpp
@@ -15,5 +15,25 @@ void uni_two_merge(const char * str1, const char * str2, size_t size, char * res
   }
   res[i] = '\0';
 }
-int main()
-{}
+int main() {
+  int const max_size = 256;
+  char str1[max_size] = {};
+  char a = 'a';
+  size_t i = 0;
+  while(std::cin >> a) {
+    str1[i] = a;
+    i++;
+  }
+  if (std::cin.fail()) {
+    return 1;
+  }
+  char * str2 = {"def "};
+  while(str2 != '\0') {
+    i++;
+  }
+  char * res = reinterpret_cast<char*>(malloc(sizeof(char) * i));
+  if (res == nullptr) {
+    return 2;
+  }
+  uni_two_merge(str1, str2, i, res);
+}
