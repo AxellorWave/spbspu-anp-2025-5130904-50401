d632cb8cccd067d3d154203d90cb56a6122167c6
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-11-22 21:17:59 +0300

    function to merge two strings

--- madieva.tatyana/P4/main.cpp
+++ madieva.tatyana/P4/main.cpp
@@ -1,2 +1,19 @@
+#include <iostream>
+void uni_two_merge(const char * str1, const char * str2, size_t size, char * res) {
+  size_t i = 0, k = 0, l = 0;
+  while(i < size - 1) {
+    if (str1[k] != '\0') {
+      res[i] = str1[k];
+      k++;
+      i++;
+    }
+    if (str2[l] != '\0') {
+      res[i] = str2[l];
+      i++;
+      l++;
+    }
+  }
+  res[i] = '\0';
+}
 int main()
 {}
