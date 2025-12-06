2c9e4357853a40d786c88339a03080bee30ca9c6
aaaa24 <chernov.arseniy@yandex.ru>
2025-12-06 21:56:05 +0300

    created namespace chernov, struct Shape

--- chernov.arseniy/P5/main.cpp
+++ chernov.arseniy/P5/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
-struct point_t {
-  double x, y;
-};
+namespace chernov {
+  struct point_t {
+    double x, y;
+  };
 
-struct rectangle_t {
-  double width, height, pos;
-};
+  struct rectangle_t {
+    double width, height, pos;
+  };
+
+  struct Shape {
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t p) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
+  };
+}
 
 int main()
 {}
