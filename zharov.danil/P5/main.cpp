70bde06f63bfcb2d5408c7a22cb15200911c6279
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-12-07 15:17:12 +0300

    added namespace zharov and structs point_t, rectangle_t, Shape

--- zharov.danil/P5/main.cpp
+++ zharov.danil/P5/main.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+
+namespace zharov {
+  struct point_t {
+    double x, y;
+  };
+
+  struct rectangle_t {
+    double width, height;
+    point_t pos;
+  };
+  
+  struct Shape {
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t p) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
+    virtual ~Shape() = default;
+  };
+}
+
 int main()
 {}
