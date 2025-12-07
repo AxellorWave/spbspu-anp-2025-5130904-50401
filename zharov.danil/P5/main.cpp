ce2e8d0091c0b6e1da58dea77f194df0f4c4fbd4
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-12-07 16:05:52 +0300

    added struct Rectangle, methods getArea(), getFrameRect(), move(), scale()

--- zharov.danil/P5/main.cpp
+++ zharov.danil/P5/main.cpp
@@ -18,6 +18,55 @@ namespace zharov {
     virtual void scale(double k) = 0;
     virtual ~Shape() = default;
   };
+
+  struct Rectangle: Shape {
+    Rectangle(double width, double height, point_t pos);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+  private:
+    double width_, height_;
+    point_t pos_;
+  };
+}
+
+zharov::Rectangle::Rectangle(double width, double height, point_t pos):
+  width_(width),
+  height_(height),
+  pos_(pos)
+{}
+
+double zharov::Rectangle::getArea() const
+{
+  return width_ * height_;
+}
+
+zharov::rectangle_t zharov::Rectangle::getFrameRect() const
+{
+  rectangle_t frame_rect;
+  frame_rect.width = width_;
+  frame_rect.height = height_;
+  frame_rect.pos = pos_;
+  return frame_rect;
+}
+
+void zharov::Rectangle::move(point_t p)
+{
+  pos_ = p;
+}
+
+void zharov::Rectangle::move(double dx, double dy)
+{
+  pos_.x += dx;
+  pos_.y += dy;
+}
+
+void zharov::Rectangle::scale(double k)
+{
+  width_ *= k;
+  height_ *= k;
 }
 
 int main()
