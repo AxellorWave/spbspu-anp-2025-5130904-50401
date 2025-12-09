7347ed0f44b3cc032bf02aee978a29ad6febf604
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-12-10 00:55:19 +0300

    add Shape

--- kuznetsov.petr/P5/main.cpp
+++ kuznetsov.petr/P5/main.cpp
@@ -9,6 +9,15 @@ namespace kuznetsov {
     double width, height;
     point_t pos;
   };
+
+  struct Shape {
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t p) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double m) = 0;
+  };
+
 }
 
 
