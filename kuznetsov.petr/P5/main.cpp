c8125f0516bb0e496550916ee27728a2b0bdf017
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-12-10 01:53:13 +0300

    difined scaleByPnt, getSumArea, getGenericFrame

--- kuznetsov.petr/P5/main.cpp
+++ kuznetsov.petr/P5/main.cpp
@@ -31,7 +31,7 @@ namespace kuznetsov {
     void scale(double m) override;
   };
 
-  void scaleByPnt(Shape** f, size_t size, point_t p, double m);
+  void scaleByPnt(Shape** fs, size_t size, point_t p, double m);
   double getSumArea(Shape** array, size_t size);
   rectangle_t getGenericFrame(Shape** array, size_t size);
 
@@ -75,5 +75,50 @@ void kuznetsov::Rectangle::scale(double m)
 {
   width_ *= m;
   height_ *= m;
+}
+
+void kuznetsov::scaleByPnt(Shape** fs, size_t size, point_t p, double m)
+{
+  for (size_t i = 0; i < size; ++i) {
+    point_t c = fs[i]->getFrameRect().pos;
+    double dx = (c.x - p.x) * (m - 1);
+    double dy = (c.y - p.y) * (m - 1);
+    fs[i]->move(dx, dy);
+    fs[i]->scale(m);
+  }
+}
+
+double kuznetsov::getSumArea(Shape** array, size_t size)
+{
+  double finalArea = 0.0;
+  for (size_t i = 0; i < size; ++i) {
+    finalArea += array[i]->getArea();
+  }
+  return finalArea;
+}
+
+kuznetsov::rectangle_t kuznetsov::getGenericFrame(Shape** array, size_t size)
+{
+  rectangle_t genericFrame;
+  rectangle_t fr = array[0]->getFrameRect();
+  double maxY = fr.pos.y + fr.height / 2;
+  double minY = fr.pos.y - fr.height / 2;
+  double maxX = fr.pos.x + fr.width / 2;
+  double minX = fr.pos.x - fr.width / 2;
+
+  for (size_t i = 1; i < size; ++i) {
+    Shape* f = array[i];
+    fr = f->getFrameRect();
+    maxY = std::max(maxY, fr.pos.y + fr.height / 2);
+    maxX = std::max(maxX, fr.pos.x + fr.width / 2);
+    minY = std::min(minY, fr.pos.y - fr.height / 2);
+    minX = std::min(minX, fr.pos.x - fr.width / 2);
+  }
+  double cx = (maxX + minX) / 2;
+  double cy = (maxY + minY) / 2;
+  genericFrame.width = maxX - minX;
+  genericFrame.height = maxY - minY;
+  genericFrame.pos = {cx, cy};
+  return genericFrame;
 }
 
