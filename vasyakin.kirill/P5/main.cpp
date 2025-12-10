8dddfb6230236b18e521094164aaf73d96f461e1
potomnazovy <skeleton39876@mail.ru>
2025-12-10 20:40:40 +0300

    add struct shape

--- vasyakin.kirill/P5/main.cpp
+++ vasyakin.kirill/P5/main.cpp
@@ -11,6 +11,16 @@ struct rectangle_t
   point_t pos;
 };
 
+struct Shape
+{
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(point_t p) = 0;
+  virtual void move(double dx, double dy) = 0;
+  virtual void scale(double k) = 0;
+  virtual ~Shape() = default;
+};
+
 int main()
 {
   return 0;
