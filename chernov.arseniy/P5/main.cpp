4c7f934262e3008eae93808d491441bbe571231e
aaaa24 <chernov.arseniy@yandex.ru>
2025-12-10 09:38:13 +0300

    added Rectangle

--- chernov.arseniy/P5/main.cpp
+++ chernov.arseniy/P5/main.cpp
@@ -6,7 +6,8 @@ namespace chernov {
   };
 
   struct rectangle_t {
-    double width, height, pos;
+    double width, height;
+    point_t pos;
   };
 
   struct Shape {
@@ -16,7 +17,50 @@ namespace chernov {
     virtual void move(double dx, double dy) = 0;
     virtual void scale(double k) = 0;
   };
+
+  struct Rectangle: Shape {
+    Rectangle(double a, double b, point_t o);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+    double side_a, side_b;
+    point_t center;
+  };
 }
 
 int main()
 {}
+
+chernov::Rectangle::Rectangle(double a, double b, point_t o):
+  side_a(a),
+  side_b(b),
+  center(o)
+{}
+
+double chernov::Rectangle::getArea() const
+{
+  return side_a * side_b;
+}
+
+chernov::rectangle_t chernov::Rectangle::getFrameRect() const
+{
+  return {side_a, side_b, center};
+}
+
+void chernov::Rectangle::move(point_t p)
+{
+  center = p;
+}
+
+void chernov::Rectangle::move(double dx, double dy)
+{
+  move({center.x + dx, center.y + dy});
+}
+
+void chernov::Rectangle::scale(double k)
+{
+  side_a *= k;
+  side_b *= k;
+}
