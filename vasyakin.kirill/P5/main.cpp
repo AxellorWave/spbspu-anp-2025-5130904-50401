ebd2157250573499ec51cc70e80f9b4be1bc6c09
potomnazovy <skeleton39876@mail.ru>
2025-12-10 22:41:41 +0300

    add struct triangle and methods and constructor for him

--- vasyakin.kirill/P5/main.cpp
+++ vasyakin.kirill/P5/main.cpp
@@ -34,6 +34,19 @@ private:
   point_t pos_;
 };
 
+struct Triangle : Shape
+{
+  Triangle(point_t a, point_t b, point_t c);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t& p) override;
+  void move(double dx, double dy) override;
+  void scale(double k) override;
+  point_t getCenter() const;
+private:
+  point_t a_, b_, c_;
+};
+
 Rectangle::Rectangle(double width, double height, point_t pos) :
   width_(width),
   height_(height),
@@ -69,6 +82,68 @@ void Rectangle::scale(double k)
 {
   width_ *= k;
   height_ *= k;
+}
+
+Triangle::Triangle(point_t a, point_t b, point_t c) :
+  a_(a),
+  b_(b),
+  c_(c)
+{}
+
+point_t Triangle::getCenter() const
+{
+  point_t center;
+  center.x = (a_.x + b_.x + c_.x) / 3.0;
+  center.y = (a_.y + b_.y + c_.y) / 3.0;
+  return center;
+}
+
+double Triangle::getArea() const
+{
+  return 0.5 * std::abs((b_.x - a_.x) * (c_.y - a_.y) - (c_.x - a_.x) * (b_.y - a_.y));
+}
+
+rectangle_t Triangle::getFrameRect() const
+{
+  rectangle_t fr_rect;
+  double min_x = std::min({a_.x, b_.x, c_.x});
+  double max_x = std::max({a_.x, b_.x, c_.x});
+  double min_y = std::min({a_.y, b_.y, c_.y});
+  double max_y = std::max({a_.y, b_.y, c_.y});
+  fr_rect.pos.x = (min_x + max_x) / 2.0;
+  fr_rect.pos.y = (min_y + max_y) / 2.0;
+  fr_rect.width = max_x - min_x;
+  fr_rect.height = max_y - min_y;
+  return fr_rect;
+}
+
+void Triangle::move(const point_t& p)
+{
+  point_t center = getCenter();
+  double dx = p.x - center.x;
+  double dy = p.y - center.y;
+  move(dx, dy);
+}
+
+void Triangle::move(double dx, double dy)
+{
+  a_.x += dx;
+  a_.y += dy;
+  b_.x += dx;
+  b_.y += dy;
+  c_.x += dx;
+  c_.y += dy;
+}
+
+void Triangle::scale(double k)
+{
+  point_t center = getCenter();
+  a_.x = center.x + (a_.x - center.x) * k;
+  a_.y = center.y + (a_.y - center.y) * k;
+  b_.x = center.x + (b_.x - center.x) * k;
+  b_.y = center.y + (b_.y - center.y) * k;
+  c_.x = center.x + (c_.x - center.x) * k;
+  c_.y = center.y + (c_.y - center.y) * k;
 }
 
 int main()
