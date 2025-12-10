b45f3663e307aa3ab1516677a9bea378e5de4beb
potomnazovy <skeleton39876@mail.ru>
2025-12-10 21:13:43 +0300

    add constructor rectangle and getArea and getFrameRect

--- vasyakin.kirill/P5/main.cpp
+++ vasyakin.kirill/P5/main.cpp
@@ -15,7 +15,7 @@ struct Shape
 {
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
-  virtual void move(point_t p) = 0;
+  virtual void move(const point_t& p) = 0;
   virtual void move(double dx, double dy) = 0;
   virtual void scale(double k) = 0;
   virtual ~Shape() = default;
@@ -26,13 +26,33 @@ struct Rectangle : Shape
   Rectangle(double width, double height, point_t pos);
   double getArea() const override;
   rectangle_t getFrameRect() const override;
-  void move(point_t p) override;
+  void move(const point_t& p) override;
   void move(double dx, double dy) override;
   void scale(double k) override;
 private:
   double width_, height_;
   point_t pos_;
 };
+
+Rectangle::Rectangle(double width, double height, point_t pos) :
+  width_(width),
+  height_(height),
+  pos_(pos)
+{}
+
+double Rectangle::getArea() const
+{
+  return width_ * height_;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  rectangle_t fr_rect;
+  fr_rect.width = width_;
+  fr_rect.height = height_;
+  fr_rect.pos = pos_;
+  return fr_rect;
+}
 
 int main()
 {
