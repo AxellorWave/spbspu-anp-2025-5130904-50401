60f4461ce29d52c4b3299cf0989c319ef4e4757c
aaaa24 <chernov.arseniy@yandex.ru>
2025-12-11 08:31:22 +0300

    added Polygon

--- chernov.arseniy/P5/main.cpp
+++ chernov.arseniy/P5/main.cpp
@@ -30,6 +30,25 @@ namespace chernov {
     point_t center;
   };
 
+  struct Polygon: Shape {
+    Polygon(point_t * points, size_t size);
+    ~Polygon() override;
+    Polygon(const Polygon & p);
+    Polygon & operator=(const Polygon & p);
+    Polygon(Polygon && p);
+    Polygon & operator=(Polygon && p);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+    double getSignedArea() const;
+    point_t getCentroid() const;
+    point_t * verts;
+    size_t count;
+    point_t center;
+  };
+
   void scaleByPoint(Shape ** shapes, size_t count, double k, point_t p);
   rectangle_t getTotalFrameRect(const Shape * const * shapes, size_t count);
   std::ostream & printShapeInfo(std::ostream & out, const Shape * shape, const char * name);
@@ -43,7 +62,7 @@ int main()
   std::ostream & output = std::cout;
   int result = 0;
 
-  const size_t count = 2;
+  const size_t count = 4;
   Shape * shapes[count];
   const char * names[count];
 
@@ -52,6 +71,16 @@ int main()
 
   shapes[1] = new Rectangle(10, 2, {-10, 3});
   names[1] = "Rectangle 2";
+
+  point_t points1[] = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+  size_t points_count = 4;
+  shapes[2] = new Polygon(points1, points_count);
+  names[2] = "Polygon 1";
+
+  point_t points2[] = {{0, 0}, {4, 1}, {5, 4}, {5, 8}, {4, 10}, {3, 8}, {2, 5}, {-1, 1}};
+  points_count = 8;
+  shapes[3] = new Polygon(points2, points_count);
+  names[3] = "Polygon 2";
 
   printShapesInfo(output, shapes, names, count);
 
@@ -143,6 +172,7 @@ std::ostream & chernov::printShapesInfo(std::ostream & out, const Shape * const 
 }
 
 chernov::Rectangle::Rectangle(double a, double b, point_t o):
+  Shape(),
   side_x(a),
   side_y(b),
   center(o)
@@ -172,4 +202,145 @@ void chernov::Rectangle::scale(double k)
 {
   side_x *= k;
   side_y *= k;
+}
+
+chernov::Polygon::Polygon(point_t * points, size_t size):
+  Shape(),
+  verts(new point_t[size]),
+  count(size),
+  center({0, 0})  
+{
+  for (size_t i = 0; i < count; ++i) {
+    verts[i] = points[i];
+  }
+  center = getCentroid();
+}
+
+chernov::Polygon::~Polygon()
+{
+  delete [] verts;
+}
+
+chernov::Polygon::Polygon(const Polygon & p):
+  Shape(),
+  verts(new point_t[p.count]),
+  count(p.count),
+  center(p.center)
+{
+  for (size_t i = 0; i < count; ++i) {
+    verts[i] = p.verts[i];
+  }
+}
+
+chernov::Polygon & chernov::Polygon::operator=(const Polygon & p)
+{
+  if (this == &p) {
+    return *this;
+  }
+  point_t * points = new point_t[p.count];
+  for (size_t i = 0; i < p.count; ++i) {
+    points[i] = p.verts[i];
+  }
+  delete [] verts;
+  verts = points;
+  count = p.count;
+  center = p.center;
+  return *this;
+}
+
+chernov::Polygon::Polygon(Polygon && p):
+  Shape(),
+  verts(p.verts),
+  count(p.count),
+  center(p.center)
+{
+  p.verts = nullptr;
+}
+
+chernov::Polygon & chernov::Polygon::operator=(Polygon && p)
+{
+  if (this == &p) {
+    return *this;
+  }
+  delete [] verts;
+  verts = p.verts;
+  count = p.count;
+  center = p.center;
+  p.verts = nullptr;
+  return *this;
+}
+
+double chernov::Polygon::getSignedArea() const
+{
+  double area = 0;
+  for (size_t i = 0; i < count; ++i) {
+    area += verts[i].x * verts[(i + 1) % count].y;
+    area -= verts[i].y * verts[(i + 1) % count].x;
+  }
+  area *= 0.5;
+  return area;
+}
+
+double chernov::Polygon::getArea() const
+{
+  return std::abs(getSignedArea());
+}
+
+chernov::rectangle_t chernov::Polygon::getFrameRect() const
+{
+  double min_x = verts[0].x, min_y = verts[0].y;
+  double max_x = min_x, max_y = min_y;
+  for (size_t i = 0; i < count; ++i) {
+    min_x = std::min(min_x, verts[i].x);
+    min_y = std::min(min_y, verts[i].y);
+    max_x = std::max(max_x, verts[i].x);
+    max_y = std::max(max_y, verts[i].y);
+  }
+  double width = max_x - min_x;
+  double height = max_y - min_y;
+  point_t pos = {min_x + width / 2, min_y + height / 2};
+  return {width, height, pos};
+}
+
+void chernov::Polygon::move(point_t p)
+{
+  double dx = p.x - center.x;
+  double dy = p.y - center.y;
+  move(dx, dy);
+}
+
+void chernov::Polygon::move(double dx, double dy)
+{
+  for (size_t i = 0; i < count; ++i) {
+    verts[i].x += dx;
+    verts[i].y += dy;
+  }
+  center.x += dx;
+  center.y += dy;
+}
+
+void chernov::Polygon::scale(double k)
+{
+  for (size_t i = 0; i < count; ++i) {
+    double dx = k * (verts[i].x - center.x);
+    double dy = k * (verts[i].y - center.y);
+    verts[i].x = center.x + dx;
+    verts[i].y = center.y + dy;
+  }
+}
+
+chernov::point_t chernov::Polygon::getCentroid() const
+{
+  double cx = 0.0, cy = 0.0;
+  for (size_t i = 0; i < count; ++i) {
+    double x0 = verts[i].x, y0 = verts[i].y;
+    double x1 = verts[(i + 1) % count].x, y1 = verts[(i + 1) % count].y;
+    double cross = x0 * y1 - x1 * y0;
+    cx += (x0 + x1) * cross;
+    cy += (y0 + y1) * cross;
+  }
+  double signed_area = getSignedArea();
+  cx /= 6 * signed_area;
+  cy /= 6 * signed_area;
+  return {cx, cy};
 }
