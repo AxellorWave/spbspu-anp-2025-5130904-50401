84879ef00dce32a952bcb37ec0e5f08cff617c4b
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-01-22 23:54:32 +0300

    split P4 to files

---
+++ zharov.danil/P4/resize.hpp
@@ -0,0 +1,12 @@
+#ifndef RESIZE_HPP
+#define RESIZE_HPP
+
+#include <cstddef>
+
+namespace zharov
+{
+  void extendArr(char *** str, size_t old_size, size_t new_size);
+  void extendStr(char ** str, size_t old_size, size_t new_size);
+  void destroyArr(char ** arr, size_t len);
+}
+#endif
